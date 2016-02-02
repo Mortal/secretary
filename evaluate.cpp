5b99b9d6a403be923361a12a0530e18f36db5f77
Mathias Rav <rav@cs.au.dk>
2016-02-02 22:12:19 +0100

    Remove unused function

--- evaluate.cpp
+++ evaluate.cpp
@@ -3,29 +3,6 @@
 #include "secretary.h"
 #include <sstream>
 
-void find_best_decision(size_t n) {
-    score_strategy_cached c;
-    std::vector<int> best(n);
-    best[n-1] = n;
-    double bestScore = c(best);
-    for (size_t i = n-1; i--;) {
-        std::vector<int> decision = best;
-        for (size_t j = 0; j <= i + 1 && j <= (size_t) best[i+1]; ++j) {
-            decision[i] = j;
-            print_decision(decision);
-            auto score = c(decision);
-            if (score < bestScore) {
-                best = decision;
-                bestScore = score;
-            } else if (score > bestScore) {
-                break;
-            }
-        }
-    }
-    print_decision(best);
-    std::cout << ' ' << bestScore << std::endl;
-}
-
 int main() {
     std::string line;
     std::vector<int> decision;
