d9cadc131e0990ab722350d97ff2b60de22765cd
Mathias Rav <rav@cs.au.dk>
2016-02-03 14:23:34 +0100

    Test char and long long

--- performance.cpp
+++ performance.cpp
@@ -28,8 +28,13 @@ double measure_time(size_t n, size_t ops) {
 int main() {
     for (size_t n = 2; n <= 20; ++n) {
         size_t ops = 100000000;
-        auto t = measure_time<int>(n, ops);
+        double
+        t = measure_time<int>(n, ops);
         std::cout << "type=int n=" << n << " ops=" << ops << " time=" << t << " s time/op=" << (1e9 * t / ops) << "ns" << std::endl;
+        t = measure_time<char>(n, ops);
+        std::cout << "type=char n=" << n << " ops=" << ops << " time=" << t << " s time/op=" << (1e9 * t / ops) << "ns" << std::endl;
+        t = measure_time<long long>(n, ops);
+        std::cout << "type=longlong n=" << n << " ops=" << ops << " time=" << t << " s time/op=" << (1e9 * t / ops) << "ns" << std::endl;
     }
     return 0;
 }
